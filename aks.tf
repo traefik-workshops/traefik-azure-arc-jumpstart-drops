07c85bab743dc435cb8dd2895f2ea2348fa9bf56
Zaid Albirawi <zalbiraw@gmail.com>
2025-05-15 13:00:50 -0400

    Update aks default values to prevent updates

--- aks.tf
+++ aks.tf
@@ -14,6 +14,12 @@ resource "azurerm_kubernetes_cluster" "traefik_demo" {
     name       = "default"
     node_count = 1
     vm_size    = var.aks_cluster_machine_type
+
+    upgrade_settings {
+      drain_timeout_in_minutes      = 0
+      max_surge                     = "10%"
+      node_soak_duration_in_minutes = 0
+    }
   }
 
   identity {
